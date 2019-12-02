6b6a272d35e858f88c8732b835f03e94f3ba3ab8
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-02 16:34:57 +0800

    Create action for fetching current user data
    - To be used in persisting the redux store during page refreshes
    (fetching of data by _app.js page)

--- src/store/actions/currentUser.js
+++ src/store/actions/currentUser.js
@@ -1,4 +1,4 @@
-import { SET_CURRENT_USER, ASYNC_LOG_USER } from './types';
+import { SET_CURRENT_USER, ASYNC_LOG_USER, ASYNC_FETCH_CURRENT_USER_DATA } from './types';
 
 export const setCurrentUser = (currentUser) => ({
   type: SET_CURRENT_USER,
@@ -8,4 +8,9 @@ export const setCurrentUser = (currentUser) => ({
 export const asyncLogUser = (params) => ({
   type: ASYNC_LOG_USER,
   params,
+});
+
+export const asyncFetchCurrentUserData = (id) => ({
+  type: ASYNC_FETCH_CURRENT_USER_DATA,
+  id,
 });
