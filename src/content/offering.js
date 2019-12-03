db65fe088940455c51f118cb0fbaddb9fb65b023
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-03 18:01:22 +0800

    Create offering content

---
+++ src/content/offering.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    title: 'Appointment Booking',
+    image: 'https://tinyimg.io/i/QHktucO.jpg',
+    text: 'Book appointments with specialists online',
+  },
+  {
+    title: 'Tele-consultation',
+    image: 'https://tinyimg.io/i/uMF8mA4.jpg',
+    text: 'Consult overseas specialists anytime, anywher',
+  },
+  {
+    title: 'Search Specialists',
+    image: 'http://tinyimg.io/i/JX71orc.png',
+    text: 'Search our vast network of specialists.',
+  },
+  {
+    title: 'Itinerary Setting',
+    image: 'https://tinyimg.io/i/jkxo1Cw.jpg',
+    text: 'We will make sure your medical trip is hassle-free.',
+  },
+];
+
