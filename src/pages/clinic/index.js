0c0af06660187a28912c884302581550f71de4ae
leon <leon_wan23@hotmail.com>
2019-12-03 22:22:25 +0800

    added loading logic for clinic main page

--- src/pages/clinic/index.js
+++ src/pages/clinic/index.js
@@ -10,25 +10,29 @@ class Clinic extends Component {
         getAdminProfileByUserIdAndClinicData(token, data)
     }
     render() {
-        const { data, clinic } = this.props;
+        const { data, clinic, loadingClinic } = this.props;
         console.log(this.props)
         return (
-            <div>
-                {data.firstName}
-                {data.lastName}
-                {clinic.id}
-            </div>
+            loadingClinic ? (
+                <div>Loading...</div>
+            ) : (
+                    <div>
+                        {clinic && clinic.id}
+                        {clinic && clinic.name}
+                    </div>
+                )
         )
     }
 }
 
 function mapStateToProps(state) {
     const { token, data } = state.currentUser;
-    const { clinic } = state.clinicReducers;
+    const { clinic, loadingClinic } = state.clinicReducers;
     return {
         data,
         token,
-        clinic
+        clinic,
+        loadingClinic
     };
 }
 
