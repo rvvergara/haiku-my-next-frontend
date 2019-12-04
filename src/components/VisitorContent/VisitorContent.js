c94ffa672c9144152288dff600be8f8096486028
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-04 08:40:46 +0800

    Temporarily add Login button

--- src/components/VisitorContent/VisitorContent.js
+++ src/components/VisitorContent/VisitorContent.js
@@ -1,9 +1,11 @@
 import Intro from './Intro';
 import About from './About';
 import OfferingList from './Offerings/OfferingsList';
+import VisitorHeader from './VisitorHeader';
 
 const VisitorContent = () => (
   <div className="visitor-content__body">
+    <VisitorHeader />
     <Intro />
     <About />
     <OfferingList />
