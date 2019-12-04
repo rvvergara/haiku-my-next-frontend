ea98f6047cb1ea5dea26e451ff005aabf75fcd48
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-04 12:39:22 +0800

    Conditionally show profile in Dashboard
    - depending on whether the currentUser is a practitioner or a patient

--- src/components/Authenticated/Dashboard.js
+++ src/components/Authenticated/Dashboard.js
@@ -1,9 +1,20 @@
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import PractitionerProfile from './Practitioner/PractitionerProfile';
 import PatientProfile from './Patient/PatientProfile';
 
-const Dashboard = () => (
+export const Dashboard = ({ userRole }) => (
   <div className="dashboard">
-    <PatientProfile />
+    {userRole === 'practitioner' ? <PractitionerProfile /> : <PatientProfile />}
   </div>
 );
 
-export default Dashboard;
+Dashboard.propTypes = {
+  userRole: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userRole: state.currentUser.data.role,
+});
+
+export default connect(mapStateToProps)(Dashboard);
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,13 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Layout from '../components/Layouts/Layout';
 import VisitorContent from '../components/VisitorContent/VisitorContent';
-import Dashboard from '../components/Authenticated/Dashboard';
+import ConnectedDashboard from '../components/Authenticated/Dashboard';
 
 export const IndexPage = ({ currentUser }) => {
   if (currentUser.authenticated) {
     return (
       <Layout title="Home">
-        <Dashboard />
+        <ConnectedDashboard />
       </Layout>
     );
   }
