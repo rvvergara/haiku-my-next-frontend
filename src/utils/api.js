ca0eaf1f736f4b0fc13018b5a44f42a437149c19
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-04 17:45:34 +0800

    Add dotenv file to read .env file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const sendRequest = async (method, path, data) => {
-  const baseUrl = process.env.API_URL || 'http://localhost:8000';
+  const baseUrl = process.env.API_URL;
   const result = await axios[method](`${baseUrl}/${path}`, data);
   return result;
 };
