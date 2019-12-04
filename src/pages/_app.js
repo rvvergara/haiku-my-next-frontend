5badeabf8906970e219b1be6429c9e27cbaed64d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-04 11:02:48 +0800

    Revise store and _app.js
    - Remove all reference to redux-saga and redux-persist

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,11 @@
 import { Provider } from 'react-redux';
 import App from 'next/app';
 import withRedux from 'next-redux-wrapper';
-import { PersistGate } from 'redux-persist/integration/react';
-import decode from 'jwt-decode';
 import initializeStore from '../store/initializeStore';
-import { setCurrentUser, asyncFetchCurrentUserData } from '../store/actions/user';
-import { setAuthorizationToken } from '../utils/api';
 import '../scss/main.scss';
 
 class IgakuApp extends App {
   static async getInitialProps({ Component, ctx }) {
-    const { store, req } = ctx;
-    if (req) {
-      try {
-        const { cookie } = req.headers;
-        const token = cookie.split('=')[1];
-        const id = decode(token).user_id;
-        store.dispatch(asyncFetchCurrentUserData(id));
-        setAuthorizationToken(token);
-      } catch (err) {
-        store.dispatch(setCurrentUser({ authenticated: false, data: {} }));
-      }
-    }
     const pageProps = Component.getInitialProps
       ? await Component.getInitialProps(ctx)
       : {};
@@ -33,9 +17,7 @@ class IgakuApp extends App {
     const { Component, pageProps, store } = this.props;
     return (
       <Provider store={store}>
-        <PersistGate persistor={store.__PERSISTOR} loading={null}>
-          <Component {...pageProps} />
-        </PersistGate>
+        <Component {...pageProps} />
       </Provider>
     );
   }
