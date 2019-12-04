18e5fd2d7fba68bca2596a4bc743c3d14b2bad12
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-04 18:39:32 +0800

    Connect PractitionerProfile to redux

--- src/components/Authenticated/Practitioner/PractitionerProfile.js
+++ src/components/Authenticated/Practitioner/PractitionerProfile.js
@@ -1,14 +1,32 @@
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import ProfileInfoGroup from '../ProfileCommon/ProfileInfoGroup';
 
-const PractitionerProfile = () => (
-  <div className="profile">
-    <ProfileInfoGroup infoKey="Name" val="Dr. Frankenstein" />
-    <ProfileInfoGroup infoKey="Clinic" val="Health Clinic" />
-    <ProfileInfoGroup infoKey="Education" val="University, Veterinary Medicine, Clinical Psychology" />
-    <ProfileInfoGroup infoKey="Specialization" val="Neurology, Orthopaedics" />
-    <ProfileInfoGroup infoKey="Years of Experience" val="23" />
-    <ProfileInfoGroup infoKey="Description" val="I am a cool doctor." />
-  </div>
-);
+export const PractitionerProfile = ({ currentUserData }) => {
+  const {
+    firstName,
+    lastName,
+  } = currentUserData;
 
-export default PractitionerProfile;
+  const fullName = `${firstName} ${lastName}`;
+  return (
+    <div className="profile">
+      <ProfileInfoGroup infoKey="Name" val={fullName} />
+      <ProfileInfoGroup infoKey="Clinic" val="Health Clinic" />
+      <ProfileInfoGroup infoKey="Education" val="University, Veterinary Medicine, Clinical Psychology" />
+      <ProfileInfoGroup infoKey="Specialization" val="Neurology, Orthopaedics" />
+      <ProfileInfoGroup infoKey="Years of Experience" val="23" />
+      <ProfileInfoGroup infoKey="Description" val="I am a cool doctor." />
+    </div>
+);
+};
+
+PractitionerProfile.propTypes = {
+  currentUserData: PropTypes.instanceOf(Object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  currentUserData: state.currentUser.data,
+});
+
+export default connect(mapStateToProps)(PractitionerProfile);
