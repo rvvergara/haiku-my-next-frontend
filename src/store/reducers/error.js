b7d5d821e7d54ea4e10450787bab963b9b37c786
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-05 13:28:25 +0800

    Set store error initial value to empty string

--- src/store/reducers/error.js
+++ src/store/reducers/error.js
@@ -1,6 +1,6 @@
 import { SET_ERROR } from '../actions/types';
 
-export default (state = null, action) => {
+export default (state = '', action) => {
   if (action.type === SET_ERROR) {
     return action.error;
   }
