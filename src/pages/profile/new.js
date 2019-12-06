131f16ad7de6394709a59cc8b989bf62327d4173
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-06 08:09:52 +0800

    Rename edit page into new page

--- src/pages/profile/new.js
+++ src/pages/profile/new.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import ConnectedPatientProfileEdit from '../../components/Authenticated/Patient/PatientProfileEdit';
+import ConnectedPatientProfileForm from '../../components/Authenticated/Patient/PatientProfileForm';
 
 const ProfileEditPage = ({ token }) => (
   <div>
-    <h1>Edit your profile first!</h1>
-    <ConnectedPatientProfileEdit token={token} />
+    <h1>Please fill up your information first</h1>
+    <ConnectedPatientProfileForm token={token} />
   </div>
 );
 
