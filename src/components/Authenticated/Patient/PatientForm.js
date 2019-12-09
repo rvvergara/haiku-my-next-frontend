3e4f0588824283b1b6093973073349c2684d89ae
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-09 15:22:21 +0800

    Enable image uploading for Practitioner

--- src/components/Authenticated/Patient/PatientForm.js
+++ src/components/Authenticated/Patient/PatientForm.js
@@ -104,7 +104,6 @@ export const PatientForm = ({
             {' '}
           </label>
           <input
-            className="user-form__input number__input"
             type="file"
             id="profile-pic"
             onChange={(e) => {
