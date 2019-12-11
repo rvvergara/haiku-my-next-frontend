65f7bb90a274b573caa7986de07f0797e000eb01
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-11 14:38:47 +0800

    Create practitioner page

---
+++ src/pages/practitioners/[id].js
@@ -0,0 +1,11 @@
+import PractitionerProfile from '../../components/Authenticated/Practitioner/PractitionerProfile';
+import Layout from '../../components/Layouts/Layout'
+
+const DoctorProfile = () => (
+  <Layout title="Practitioner Profile">
+    <h1>DoctorProfile Practitioner Profile here</h1>
+    <PractitionerProfile/>
+  </Layout>
+);
+
+export default DoctorProfile;
