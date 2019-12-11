c2425cc8771d121f34695c80feec8afbb95b998c
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-11 10:21:35 +0700

    experiment with associated

--- src/components/Authenticated/Clinic/ClinicForm.js
+++ src/components/Authenticated/Clinic/ClinicForm.js
@@ -1,12 +1,16 @@
 import { connect } from 'react-redux';
 import { createClinic } from '../../../store/thunks/clinic';
-import { setAuthorizationToken} from '../../../utils/api';
+import { setAuthorizationToken } from '../../../utils/api';
 
 class ClinicForm extends React.Component {
+
+  // this.props.currentUserData
+
   state = {
     name: '',
     address: '',
     postalCode: '',
+    associated:false,
   };
 
   handleChange = (key, val) => {
@@ -15,18 +19,31 @@ class ClinicForm extends React.Component {
     }));
   };
 
-  handleSubmit = async e =>{
-    setAuthorizationToken(this.props.token);
-    e.preventDefault()
-    await this.props.createClinic(this.state)
+  handleAssociated = () => {
+    this.setState((prevState) => ({
+      associated:!prevState.associated
+    }))
   }
+
+  handleSubmit = async e => {
+    setAuthorizationToken(this.props.token);
+    e.preventDefault();
+    const profileId = this.props.currentUserData.profile.id
+
+
+    const params = this.state.associated ? {...this.state, practitioners:[profileId]} : this.state
+
+    await this.props.createClinic(params);
+  };
 
   render() {
     return (
       <div className="container profile-form-container">
         <form className="user-form profile-form">
           <div className="form-group">
-            <label htmlFor="clinic-name" className="auth-label">Name</label>
+            <label htmlFor="clinic-name" className="auth-label">
+              Name
+            </label>
             <input
               id="clinic-name"
               className="user-form__input"
@@ -58,12 +75,24 @@ class ClinicForm extends React.Component {
               onChange={e => this.handleChange('postalCode', e.target.value)}
             />
           </div>
+
           <div className="form-group">
-          <button
-            className="user-form__button" 
-            onClick={this.handleSubmit}>
-            Submit
-          </button>
+          <label htmlFor="associated" className="auth-label">
+              Do i work here?
+            </label>
+            <input
+            id="associated"
+              type="checkbox"
+              checked={this.state.associated}
+              onChange={this.handleAssociated}
+              className="checkbox-round"
+            ></input>
+          </div>
+
+          <div className="form-group">
+            <button className="user-form__button" onClick={this.handleSubmit}>
+              Submit
+            </button>
           </div>
         </form>
       </div>
@@ -71,6 +100,8 @@ class ClinicForm extends React.Component {
   }
 }
 
+const mapStateToProps = (state) => ({
+  currentUserData: state.currentUser.data,
+});
 
-
-export default connect(null, { createClinic })(ClinicForm);
+export default connect(mapStateToProps, { createClinic })(ClinicForm);
