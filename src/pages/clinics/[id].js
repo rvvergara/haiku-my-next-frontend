bc6f469bed091ee9ab0fb4875e6318ab5a9251fa
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-12 15:54:05 +0700

    debug fetchoneclinic

--- src/pages/clinics/[id].js
+++ src/pages/clinics/[id].js
@@ -1,18 +1,22 @@
 import { connect } from 'react-redux';
 import ClinicProfile from '../../components/Authenticated/Clinic/ClinicProfile';
 import Layout from '../../components/Layouts/Layout';
+import { fetchOneClinic } from '../../store/thunks/clinic'
+
 
 const ClinicPage = ({ clinic }) => (
+
+
   <Layout title={clinic.name}>
     <ClinicProfile />
   </Layout>
 );
 
-ClinicProfile.getInitialProps = ctx => {
+ClinicProfile.getInitialProps = async(ctx )=> {
   const { store } = ctx;
-  const clinic = store.getState().displayedClinic;
+  const clinic = await store.dispatch(fetchOneClinic(ctx.query.id))
   return { clinic };
 };
 
-export default connect((state) => state)(ClinicPage);
+export default connect((state) => state,{ fetchOneClinic })(ClinicPage);
 
