fcd6eaac7cb9e18624c065c0892621dd5a468dab
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-12 14:30:31 +0800

    Connect setClinic to ClinicProfile component

--- src/components/Authenticated/Clinic/ClinicProfile.js
+++ src/components/Authenticated/Clinic/ClinicProfile.js
@@ -1,9 +1,29 @@
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import Router from 'next/router';
+import { fetchOneClinic } from '../../../store/thunks/clinic';
+import { setClinic } from '../../../store/actions/clinic';
+import { setAuthorizationToken } from '../../../utils/api';
 
-const ClinicProfile = () => (
-  <div>
-      Hello I am a clinic
-  </div>
+const ClinicProfile = ({ fetchOneClinic, setClinic, clinic }) => {
+  useEffect(() => {
+    if (process.browser) {
+      setAuthorizationToken(localStorage.token);
+    }
+    fetchOneClinic(Router.query.id);
+    return () => {
+      setClinic({});
+    };
+  }, []);
+  return (
+    <div>
+      <h1>{clinic.name}</h1>
+    </div>
   );
+};
 
-export default connect(null, null)(ClinicProfile);
+const mapStateToProps = (state) => ({
+  clinic: state.displayedClinic,
+});
+
+export default connect(mapStateToProps, { fetchOneClinic, setClinic })(ClinicProfile);
