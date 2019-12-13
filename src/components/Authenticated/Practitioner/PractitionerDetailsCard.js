f931504a1b7a114b651c229087184de8d020801f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-13 13:16:19 +0800

    Connect PractitionerDetailsCard to store

--- src/components/Authenticated/Practitioner/PractitionerDetailsCard.js
+++ src/components/Authenticated/Practitioner/PractitionerDetailsCard.js
@@ -1,4 +1,7 @@
-const PractitionerDetailsCard = () => (
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const PractitionerDetailsCard = ({ practitioner }) => (
   <div className="practitioner-card">
     <div className="profile-info-container">
       <div className="profile-info-container__info__card container">
@@ -6,12 +9,21 @@ const PractitionerDetailsCard = () => (
           Bio
         </h4>
         <p className="grotesque-font profile-info-container__info__card__content">
-        Dr. Goh Min Yih Peter is a General Surgeon in Orchard, Singapore and has an experience of 38 years in this field. Dr. Goh Min Yih Peter practices at Advance Surgical Group Pte Ltd in Orchard, Singapore. He completed MBBS from NUS-Singapore in 1980,FRCS - General Surgery from Royal College of Surgeons of Edinburgh (RCSE), U.K in 1984 and M Med (Gen Surg) from NUS-Singapore in 1985.
-He is a member of Singapore Medical Council. Some of the services provided by the doctor are: Conventional Haemorrhoidectomy,Obesity Surgery,Gall Bladder (Biliary) Stone Treatment,Gastroscopy and Colonoscopy etc.
+          {
+            practitioner.biography
+          }
         </p>
       </div>
     </div>
   </div>
 );
 
-export default PractitionerDetailsCard;
+PractitionerDetailsCard.propTypes = {
+  practitioner: PropTypes.instanceOf(Object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  practitioner: state.displayedPractitioner,
+});
+
+export default connect(mapStateToProps)(PractitionerDetailsCard);
