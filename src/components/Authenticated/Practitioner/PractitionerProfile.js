fef1c08a2be63e94ed1223df11f4807056650b15
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-13 12:47:06 +0800

    Clean up PractitionerProfile page and component
    - Remove unnecessary setting of auth token in /practitioners/[id] page
    - Remove connect in PractitionerProfile component

--- src/components/Authenticated/Practitioner/PractitionerProfile.js
+++ src/components/Authenticated/Practitioner/PractitionerProfile.js
@@ -1,55 +1,17 @@
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import ProfileInfoGroup from '../ProfileCommon/ProfileInfoGroup';
 import PractitionerCard from './PractitionerCard';
 import PractitionerDetailsCard from './PractitionerDetailsCard';
-import Booking from '../Booking/Booking'
+import Booking from '../Booking/Booking';
 
 
-export const PractitionerProfile = ({ currentUserData }) => {
-  const {
-    firstName,
-    lastName,
-    profile,
-  } = currentUserData;
-
-  let education = ['No education info yet'];
-  let specialties = ['No specialties info yet'];
-  let yearsExp = 0;
-  let biography = 'No biography information yet';
-
-  if (profile) {
-    education = profile.education;
-    specialties = profile.specialities;
-    yearsExp = profile.yearsExp;
-    biography = profile.biography;
-  }
-
-  const fullName = `${firstName} ${lastName}`;
-  return (
-    <div className="profile">
-      {/* <ProfileInfoGroup infoKey="Name" val="Dr. Johny Walker" />
-      <ProfileInfoGroup infoKey="Clinic" val="Health Clinic" />
-      <ProfileInfoGroup infoKey="Education" val="Elementary, High School" />
-      <ProfileInfoGroup infoKey="Specialization" val="Neurology, Cardiology" />
-      <ProfileInfoGroup infoKey="Years of Experience" val="2" />
-  <ProfileInfoGroup infoKey="Biograpy" val="Hello, my patients!" /> */}
-  <div>
-  <PractitionerCard />
+const PractitionerProfile = () => (
+  <div className="profile">
+    <div>
+      <PractitionerCard />
       <PractitionerDetailsCard />
-  </div>
-
-      <Booking />
     </div>
+
+    <Booking />
+  </div>
 );
-};
 
-PractitionerProfile.propTypes = {
-  currentUserData: PropTypes.instanceOf(Object).isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  currentUserData: state.currentUser.data,
-});
-
-export default connect(mapStateToProps)(PractitionerProfile);
+export default PractitionerProfile;
--- src/pages/practitioners/[id].js
+++ src/pages/practitioners/[id].js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import PractitionerProfile from '../../components/Authenticated/Practitioner/PractitionerProfile';
 import Layout from '../../components/Layouts/Layout';
-import { setAuthorizationToken } from '../../utils/api';
 import { fetchOnePractitioner } from '../../store/thunks/practitioner';
 
 const DoctorProfile = () => (
@@ -11,14 +10,7 @@ const DoctorProfile = () => (
 );
 
 DoctorProfile.getInitialProps = async (ctx) => {
-  const { store, req, query } = ctx;
-  let token;
-  if (ctx.isServer) {
-    token = req.headers.cookie.split('=')[1];
-  } else {
-    token = store.getState().currentUser.data.token;
-  }
-  setAuthorizationToken(token);
+  const { store, query } = ctx;
   await store.dispatch(fetchOnePractitioner(query.id));
   const { data } = store.getState().currentUser;
   return { currentUserData: data };
