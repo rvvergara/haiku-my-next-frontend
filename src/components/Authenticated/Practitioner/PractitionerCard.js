9cf201dbad21d341c066306615a2b623cebc6f1d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-13 16:38:11 +0800

    Fix Practitioner homepage bug

--- src/components/Authenticated/Practitioner/PractitionerCard.js
+++ src/components/Authenticated/Practitioner/PractitionerCard.js
@@ -54,7 +54,9 @@ PractitionerCard.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  practitioner: state.displayedPractitioner,
+  practitioner: Object.keys(state.displayedPractitioner).length > 0
+  ? state.displayedPractitioner
+  : state.currentUser.data.profile,
 });
 
 export default connect(mapStateToProps)(PractitionerCard);
