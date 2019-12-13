8879b2e4b2150af3709127539cb0c1385865a47a
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-13 13:12:25 +0800

    Clean up displayedPractitioner from redux store
    - upon unmounting of practitioner page

--- src/pages/practitioners/[id].js
+++ src/pages/practitioners/[id].js
@@ -1,19 +1,29 @@
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import PractitionerProfile from '../../components/Authenticated/Practitioner/PractitionerProfile';
 import Layout from '../../components/Layouts/Layout';
 import { fetchOnePractitioner } from '../../store/thunks/practitioner';
+import { setPractitioner } from '../../store/actions/practitioners';
 
-const DoctorProfile = () => (
-  <Layout title="Practitioner Profile">
-    <PractitionerProfile />
-  </Layout>
+const DoctorProfile = ({ dispatch }) => {
+  useEffect(() => () => dispatch(setPractitioner({})), []);
+  return (
+    <Layout title="Practitioner Profile">
+      <PractitionerProfile />
+    </Layout>
 );
+};
+
+DoctorProfile.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
 
 DoctorProfile.getInitialProps = async (ctx) => {
   const { store, query } = ctx;
-  await store.dispatch(fetchOnePractitioner(query.id));
-  const { data } = store.getState().currentUser;
-  return { currentUserData: data };
+  const { dispatch } = store;
+  await dispatch(fetchOnePractitioner(query.id));
+  return { dispatch };
 };
 
 export default connect((state) => state)(DoctorProfile);
