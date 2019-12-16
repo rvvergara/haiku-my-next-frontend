012c4e974177bb8ecbf1569a6117eb2f6e0f567f
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-16 09:04:20 +0700

    add dummy availability data

---
+++ src/Dummy/availability.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    date: 'December 16,2019',
+    startTime: '10:00AM',
+    endTime: '11:00AM',
+  },
+  {
+    date: 'December 17,2019',
+    startTime: '9:00AM',
+    endTime: '10:00AM',
+  },
+  {
+    date: 'December 18,2019',
+    startTime: '10:00AM',
+    endTime: '11:00AM',
+  },
+  {
+    date: 'December 19,2019',
+    startTime: '10:00AM',
+    endTime: '11:00AM',
+  },
+];
