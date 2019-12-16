fb130b05eed9c07d089f6b7e7d9e30cce03cde02
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-16 15:27:36 +0800

    Add removeAvailability action and reducer case

--- src/store/actions/availability.js
+++ src/store/actions/availability.js
@@ -1,6 +1,11 @@
-import { LIST_AVAILABILITIES } from './types';
+import { LIST_AVAILABILITIES, REMOVE_AVAILABILITY } from './types';
 
-export const listAvailabilies = availabilities => ({
+export const listAvailabilies = (availabilities) => ({
   type: LIST_AVAILABILITIES,
   availabilities,
+});
+
+export const removeAvailability = (id) => ({
+  type: REMOVE_AVAILABILITY,
+  id,
 });
--- src/store/reducers/availabilities.js
+++ src/store/reducers/availabilities.js
@@ -1,10 +1,12 @@
 import availabilities from '../../Dummy/availabilites';
-import { LIST_AVAILABILITIES } from '../actions/types';
+import { LIST_AVAILABILITIES, REMOVE_AVAILABILITY } from '../actions/types';
 
 export default (state = availabilities, action) => {
   switch (action.type) {
     case LIST_AVAILABILITIES:
       return action.availabilities;
+    case REMOVE_AVAILABILITY:
+      return state.filter((avail) => avail.id !== action.id);
     default:
       return state;
   }
