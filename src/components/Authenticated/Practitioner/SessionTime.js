5188db2c2ce5a78e5914264e026d3d5cd88a61f3
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-17 14:12:47 +0800

    Use TimePicker on SessionTime

--- src/components/Authenticated/Practitioner/SessionTime.js
+++ src/components/Authenticated/Practitioner/SessionTime.js
@@ -1,14 +1,50 @@
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
+import moment from 'moment';
+import { setSessionStartTime } from '../../../store/actions/availability';
 
-const SessionTimes = () => (
-  <div>
-    <button type="button" className="booking-availabilities">
-        10:00AM
-    </button>
-    <button type="button" className="booking-availabilities">
-        02:00PM
-    </button>
-  </div>
+const SessionTimes = ({ sessionDuration, sessionStartTime, setSessionStartTime }) => {
+  const format = 'h:mm a';
+  const now = moment().hour(9).minute(0);
+  console.log(moment(sessionStartTime, 'HH:MM').add(sessionDuration, 'minutes').format(format));
+  const handleChange = (val) => {
+    setSessionStartTime(val.format(format));
+  };
+
+  return (
+    <div>
+      <label htmlFor="from-time">
+        Start Time:
+        {' '}
+      </label>
+      <TimePicker
+        id="from-time"
+        showSecond={false}
+        defaultValue={now}
+        className="xxx"
+        onChange={handleChange}
+        format={format}
+        use12Hours
+        inputReadOnly
+      />
+      <label htmlFor="to-time">
+        End time:
+      </label>
+      <strong>{sessionDuration}</strong>
+    </div>
   );
+};
 
-export default connect()(SessionTimes);
+SessionTimes.propTypes = {
+  sessionDuration: PropTypes.number.isRequired,
+  setSessionStartTime: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  sessionStartTime: state.sessionStartTime,
+  sessionDuration: state.sessionDuration,
+});
+
+export default connect(mapStateToProps, { setSessionStartTime })(SessionTimes);
