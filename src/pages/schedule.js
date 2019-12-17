69669e29f1e8c96a08a576f839b7e2676169292d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-17 10:22:47 +0800

    Create practitioner schedule page

---
+++ src/pages/schedule.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layouts/Layout';
+
+const SchedulePage = () => (
+  <Layout title="Schedule Availability">
+    <h1>Doctor Schedule setting here</h1>
+  </Layout>
+);
+
+export default SchedulePage;
