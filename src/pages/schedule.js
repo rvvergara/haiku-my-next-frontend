157f8fd6f7f727673a54712141660ef0de51cbde
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-17 09:59:13 +0700

    concept for the scheduler page

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -3,8 +3,26 @@ import Layout from '../components/Layouts/Layout';
 
 const SchedulePage = () => (
   <Layout title="Schedule Availability">
-    <h1>Doctor Schedule setting here</h1>
+    <h3>Choose an available day for your appointment</h3>
     <ScheduleForm />
+    <h3>Choose Duration</h3>
+    <button type="button" className="booking-availabilities">
+      30 minutes
+    </button>
+    <button type="button" className="booking-availabilities">
+      60 minutes
+    </button>
+    <button type="button" className="booking-availabilities">
+      120 minutes
+    </button>
+    <h3>Choose an available time for your appointment</h3>
+    <button type="button" className="booking-availabilities">
+      10:00AM
+    </button>
+    <button type="button" className="booking-availabilities">
+      02:00PM
+    </button>
+    <button type="button">Submit</button>
   </Layout>
 );
 
