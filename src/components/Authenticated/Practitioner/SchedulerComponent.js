e2d715db87f6b481d753bf44af303f26c7534d93
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-17 16:55:20 +0800

    Reset store upon SchedulerComponent unmount

--- src/components/Authenticated/Practitioner/SchedulerComponent.js
+++ src/components/Authenticated/Practitioner/SchedulerComponent.js
@@ -1,18 +1,30 @@
+import { useEffect } from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { setAlert } from '../../../store/actions/alerts';
 import { addBooking } from '../../../store/actions/booking';
 import ScheduleForm from './ScheduleForm';
 import SessionDuration from './SessionDuration';
 import SessionTime from './SessionTime';
+import { setSessionDate, setSessionDuration, setSessionStartTime } from '../../../store/actions/availability';
 
 const SchedulerComponent = ({
   sessionDate,
   sessionDuration,
   sessionStartTime,
   addBooking,
-  setAlert
+  setAlert,
+  setSessionDate,
+  setSessionDuration,
+  setSessionStartTime,
 }) => {
+  useEffect(() => () => {
+      setSessionDate(moment().format('MMMM D, YYYY'));
+      setSessionDuration(30);
+      setSessionStartTime('9:00 am');
+    }, []);
+
   const handleSubmit = () => {
     const endTime = moment(sessionStartTime, 'h:mm')
       .add(sessionDuration, 'minutes')
@@ -46,10 +58,27 @@ const SchedulerComponent = ({
   );
 };
 
-const mapStateToProps = state => ({
+SchedulerComponent.propTypes = {
+  sessionDate: PropTypes.string.isRequired,
+  sessionDuration: PropTypes.number.isRequired,
+  sessionStartTime: PropTypes.string.isRequired,
+  addBooking: PropTypes.func.isRequired,
+  setAlert: PropTypes.func.isRequired,
+  setSessionDate: PropTypes.func.isRequired,
+  setSessionDuration: PropTypes.func.isRequired,
+  setSessionStartTime: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
   sessionDate: state.sessionDate,
   sessionDuration: state.sessionDuration,
   sessionStartTime: state.sessionStartTime,
 });
 
-export default connect(mapStateToProps, { addBooking,setAlert })(SchedulerComponent);
+export default connect(mapStateToProps, {
+  addBooking,
+  setAlert,
+  setSessionDate,
+  setSessionDuration,
+  setSessionStartTime,
+})(SchedulerComponent);
