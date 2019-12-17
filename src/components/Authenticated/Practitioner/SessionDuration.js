c5a329123f45ea04863d65d8fe1b015e20e5fcf0
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-17 12:55:00 +0800

    Connect SessionDuration to redux store

--- src/components/Authenticated/Practitioner/SessionDuration.js
+++ src/components/Authenticated/Practitioner/SessionDuration.js
@@ -1,18 +1,46 @@
-const SessionDuration = () => (
-  <div>
-    <h3>Choose Duration</h3>
-    <div className="duration-container">
-      <button type="button" className="booking-duration">
-          30 minutes
-      </button>
-      <button type="button" className="booking-duration">
-          60 minutes
-      </button>
-      <button type="button" className="booking-duration">
-          120 minutes
-      </button>
-    </div>
-  </div>
-  );
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { setSessionDuration } from '../../../store/actions/availability';
 
-export default SessionDuration;
+const SessionDuration = ({ setSessionDuration }) => {
+  const handleClick = (e) => {
+    setSessionDuration(parseInt(e.target.value));
+  };
+  return (
+    <div>
+      <h3>Choose Duration</h3>
+      <div className="duration-container">
+        <button
+          type="button"
+          className="booking-duration"
+          value={30}
+          onClick={handleClick}
+        >
+          30 minutes
+        </button>
+        <button
+          type="button"
+          className="booking-duration"
+          value={60}
+          onClick={handleClick}
+        >
+          60 minutes
+        </button>
+        <button
+          type="button"
+          className="booking-duration"
+          value={120}
+          onClick={handleClick}
+        >
+          120 minutes
+        </button>
+      </div>
+    </div>
+  );
+};
+
+SessionDuration.propTypes = {
+  setSessionDuration: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setSessionDuration })(SessionDuration);
