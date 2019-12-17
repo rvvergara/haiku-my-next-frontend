ae6e2634893363641844e899d76db383d5bda1bf
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-17 08:46:11 +0700

    add BookingList component

--- src/components/Authenticated/Dashboard.js
+++ src/components/Authenticated/Dashboard.js
@@ -1,30 +1,34 @@
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import PractitionerProfile from './Practitioner/PractitionerProfile';
+import { connect } from 'react-redux';
+import BookingBox from './Booking/BookingList';
 import ClinicList from './Clinic/ClinicList';
+import PractitionerProfile from './Practitioner/PractitionerProfile';
 
-const renderDashboard = (userRole) => {
+const renderDashboard = userRole => {
   switch (userRole) {
     case 'practitioner':
       return <PractitionerProfile />;
     case 'patient':
-      return <ClinicList />;
+      return (
+        <div>
+          <BookingBox />
+          <ClinicList />
+        </div>
+      );
     default:
       return null;
   }
 };
 
 export const Dashboard = ({ userRole }) => (
-  <div className="dashboard">
-    {renderDashboard(userRole)}
-  </div>
+  <div className="dashboard">{renderDashboard(userRole)}</div>
 );
 
 Dashboard.propTypes = {
   userRole: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   userRole: state.currentUser.data.role,
 });
 
