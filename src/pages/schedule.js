1e4b72da2d9187eb0bc9eeb3cb25072c08dddb0d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-18 17:07:04 +0800

    Redirect patient accessing availability scheduling page

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,6 +1,7 @@
+import { connect } from 'react-redux';
+import redirect from 'next-redirect';
 import SchedulerComponent from '../components/Authenticated/Practitioner/SchedulerComponent';
 import Layout from '../components/Layouts/Layout';
-
 
 const SchedulePage = () => (
   <Layout title="Schedule Availability">
@@ -8,4 +9,12 @@ const SchedulePage = () => (
   </Layout>
 );
 
-export default SchedulePage;
+SchedulePage.getInitialProps = (ctx) => {
+  const { store } = ctx;
+  const { role } = store.getState().currentUser.data;
+  if (role !== 'practitioner') {
+    return redirect(ctx, '/');
+  }
+};
+
+export default connect((state) => state)(SchedulePage);
