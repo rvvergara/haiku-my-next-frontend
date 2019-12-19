a483324b014c5258fa41d7d993e56ffcc304736e
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-19 13:45:04 +0700

    add responsive navbar

--- src/components/VisitorContent/VisitorHeader.js
+++ src/components/VisitorContent/VisitorHeader.js
@@ -1,30 +1,34 @@
-import Link from 'next/link';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const VisitorHeader = () => (
-  <header className="visitor-header">
-    <div className="container">
-      <div className="authentication-group">
-        <Link href="/login">
-          <button
-            type="button"
-            className="authentication"
-            onClick={() => localStorage.clear()}
-          >
-            Login
-          </button>
-        </Link>
-        <Link href="/signup">
-          <button
-            type="button"
-            className="authentication"
-            onClick={() => localStorage.clear()}
-          >
-            Signup
-          </button>
-        </Link>
-      </div>
-    </div>
-  </header>
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+
+
+        <Link href="/login"
+      onClick={() => localStorage.clear()}
+    >
+        <Nav.Link href="/login">Login</Nav.Link>
+  </Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+
+
+  // <Link href="/signup">
+  //   <button
+  //     type="button"
+  //     className="authentication"
+  //     onClick={() => localStorage.clear()}
+  //   >
+  //     Signup
+  //   </button>
+  // </Link>
 );
 
 export default VisitorHeader;
