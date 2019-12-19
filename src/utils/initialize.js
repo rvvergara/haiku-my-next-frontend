9e70aee14fe7a20f6ada9f34c3bd9381d9b26e4f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-19 11:20:12 +0800

    Create checkIfTokenExp function

--- src/utils/initialize.js
+++ src/utils/initialize.js
@@ -1,5 +1,6 @@
 import decode from 'jwt-decode';
 import redirect from 'next-redirect';
+import moment from 'moment';
 import { getCookie } from './cookie';
 import { setAuthorizationToken } from './api';
 import { fetchUserData } from '../store/thunks/user';
@@ -20,26 +21,43 @@ const redirectIfNoToken = (ctx) => {
   }
 };
 
+const checkIfTokenExp = (decoded) => {
+  const expirationTime = moment.unix(decoded.exp);
+  const nowTime = moment.unix();
+  console.log('IS ALREADY EXPIRED???', expirationTime < nowTime);
+  return expirationTime < nowTime;
+};
+
 export default async (ctx) => {
   if (ctx.isServer) {
     if (ctx.req.headers.cookie) {
       const { req, store } = ctx;
       const { dispatch } = store;
       const token = getCookie('token', req);
-      const id = decode(token).user_id;
+      const decoded = decode(token);
+      if (checkIfTokenExp(decoded)) {
+        return redirectIfNoToken(ctx);
+      }
+      console.log('THIS SHOULD NOT APPEAR ANYMORE');
+      const id = decoded.user_id;
       setAuthorizationToken(token);
       await dispatch(fetchUserData(id));
       const { data } = store.getState().currentUser;
-      return redirectIfNoProfile(ctx, data);
+      // return redirectIfNoProfile(ctx, data);
     }
     redirectIfNoToken(ctx);
   } else {
     try {
       const { token } = localStorage;
       if (token) {
+        const decoded = decode(token);
+        if (checkIfTokenExp(decoded)) {
+          return redirectIfNoToken(ctx);
+        }
+        console.log('IN THE CLIENT THIS SHOULD NOT APPEAR');
         setAuthorizationToken(token);
         const { data } = ctx.store.getState().currentUser;
-        return redirectIfNoProfile(ctx, data);
+        // return redirectIfNoProfile(ctx, data);
       }
       return redirectIfNoToken(ctx);
     } catch (err) {
