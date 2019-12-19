f64eb59d4f730e538951aad21acf695ca7035cea
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-19 09:31:00 +0800

    Make ScheduleList functional

--- src/components/Authenticated/Practitioner/ScheduleList.js
+++ src/components/Authenticated/Practitioner/ScheduleList.js
@@ -1,9 +1,32 @@
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { listAvailabilies } from '../../../store/actions/availability';
+import dummyAvailabilities from '../../../Dummy/availabilites';
 
-const ScheduleList = () => (
-  <div>
-    <h1>All my schedule here</h1>
-  </div>
+const ScheduleList = ({ availabilities, listAvailabilies }) => {
+  useEffect(() => {
+    listAvailabilies(dummyAvailabilities);
+    return () => listAvailabilies([]);
+  }, []);
+  return (
+    <div>
+      {
+        availabilities.map((availability) => (
+          <div key={availability.id}><strong>{availability.date}</strong></div>
+        ))
+      }
+    </div>
 );
+};
 
-export default connect()(ScheduleList);
+ScheduleList.propTypes = {
+  availabilities: PropTypes.instanceOf(Object).isRequired,
+  listAvailabilies: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  availabilities: state.availabilities,
+});
+
+export default connect(mapStateToProps, { listAvailabilies })(ScheduleList);
--- src/store/reducers/availabilities.js
+++ src/store/reducers/availabilities.js
@@ -1,12 +1,15 @@
+import uuid from 'uuid';
 import availabilities from '../../Dummy/availabilites';
 import { LIST_AVAILABILITIES, REMOVE_AVAILABILITY, ADD_AVAILABILITY } from '../actions/types';
+
+// Temporarily setting id for new availabilities until we connect to backend
 
 export default (state = availabilities, action) => {
   switch (action.type) {
     case LIST_AVAILABILITIES:
       return action.availabilities;
     case ADD_AVAILABILITY:
-      return [...state, action.availability];
+      return [...state, { ...action.availability, id: uuid() }];
     case REMOVE_AVAILABILITY:
       return state.filter((avail) => avail.id !== action.id);
     default:
