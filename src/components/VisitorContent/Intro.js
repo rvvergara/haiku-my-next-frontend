ca470a59135e7803b2263aa243b0b092b94e0978
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-19 13:12:21 +0700

    experiment with css

--- src/components/VisitorContent/Intro.js
+++ src/components/VisitorContent/Intro.js
@@ -1,20 +1,11 @@
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 
 const Intro = () => (
   <Container>
     <Row>
-      <Col xs={6} md={4}>
-        <Image src="holder.js/171x180" rounded />
-      </Col>
-      <Col xs={6} md={4}>
-        <Image src="holder.js/171x180" roundedCircle />
-      </Col>
-      <Col xs={6} md={4}>
-        <Image src="holder.js/171x180" thumbnail />
-      </Col>
+      <Col xs={6} md={4} className="mobile-container" />
     </Row>
   </Container>
 );
