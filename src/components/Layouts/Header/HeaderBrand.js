e8a9b65b54851c11b662e18d0959b81634e4a9d1
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-20 16:39:20 +0700

    fix relative path brand image

--- src/components/Layouts/Header/HeaderBrand.js
+++ src/components/Layouts/Header/HeaderBrand.js
@@ -3,7 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 const HeaderBrand = () => (
   <Navbar.Brand href='/'>
     <img
-      src="./static/igaku_logo_side_by_side_text_shadow.png"
+      src="/static/igaku_logo_side_by_side_text_shadow.png"
       className="header-logo-text logged-header-logo-text"
       alt="Igaku Logo"
     />
