b71db8fad0244702262d4c2f6f13eece815ead5b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-25 14:21:11 +0800

    Localize fetched availability data

--- src/store/thunks/availability.js
+++ src/store/thunks/availability.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { sendRequest } from '../../utils/api';
 import { addAvailability } from '../actions/availability';
 import setError from '../actions/error';
@@ -7,8 +8,19 @@ export const createAvailabilityOnDb = (params) => async (dispatch) => {
 
   try {
     const res = await sendRequest('post', path, params);
-    dispatch(addAvailability(params));
-    return res;
+    const { booking_slot } = res.data;
+    const { date, startTime, endTime } = booking_slot;
+    const localizedDate = moment(date).format('MMMM DD, YYYY');
+    const localizedStartTime = moment(startTime).local().format('LT');
+    const localizedEndTime = moment(endTime).local().format('LT');
+    const localizedBookingSlot = {
+      ...booking_slot,
+     date: localizedDate,
+     startTime: localizedStartTime,
+     endTime: localizedEndTime,
+   };
+    dispatch(addAvailability(localizedBookingSlot));
+    return localizedBookingSlot;
   } catch (err) {
     dispatch(setError(err));
   }
