61203a419622335ca05642b591e6fcbf9ef847dc
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-27 11:25:10 +0800

    Add booking availability feature to redux store

--- src/store/reducers/availabilities.js
+++ src/store/reducers/availabilities.js
@@ -3,6 +3,7 @@ import {
   ADD_AVAILABILITY,
   LIST_AVAILABILITIES,
   REMOVE_AVAILABILITY,
+  BOOK_AVAILABILITY,
 } from '../actions/types';
 
 // Temporarily setting id for new availabilities until we connect to backend
@@ -15,6 +16,14 @@ export default (state = [], action) => {
       return [...state, { ...action.availability, id: uuid(), booked: false }];
     case REMOVE_AVAILABILITY:
       return state.filter((avail) => avail.id !== action.id);
+    case BOOK_AVAILABILITY:
+      {
+        const indexToBook = state.findIndex((avail) => avail.id === action.availabilityId);
+        const bookedSlot = { ...state[indexToBook], patientId: action.patientId, remarks: action.remarks };
+        const newState = [...state];
+        newState[indexToBook] = bookedSlot;
+        return newState;
+      }
     default:
       return state;
   }
