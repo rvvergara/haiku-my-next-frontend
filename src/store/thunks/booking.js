c1ebc3ff464a88c2b3ed1bd1aea3c1346eb79915
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-27 10:52:23 +0800

    Create bookSlot booking thunk

--- src/store/thunks/booking.js
+++ src/store/thunks/booking.js
@@ -1,2 +1,15 @@
 import { sendRequest } from '../../utils/api';
 import { addBooking } from '../actions/booking';
+import setError from '../actions/error';
+
+export const bookSlot = (params, slotId) => async (dispatch) => {
+  const path = `v1/booking-slots/${slotId}/book`;
+
+  try {
+    const res = await sendRequest('post', path, params);
+    const bookedSlot = await res.data.booking_slot;
+    dispatch(addBooking(bookedSlot));
+  } catch (err) {
+    dispatch(setError(err));
+  }
+};
