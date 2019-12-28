0cfa3272400526cb7daa9203a604b9f67f7e4f4d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-28 19:33:43 +0800

    Clean up user thunk

--- src/store/thunks/user.js
+++ src/store/thunks/user.js
@@ -14,32 +14,10 @@ const fetchUserProfile = async (id, role) => {
   }
 };
 
-const setUserInStore = async (user, dispatch) => {
-  const { id, role } = user;
-  const profile = await fetchUserProfile(id, role);
-  if (profile) {
-    // If user has a profile already add it to user data
-    dispatch(setCurrentUser({
-      authenticated: true,
-      data: { ...user, profile: profile.data[role.toLowerCase()], token: user.token },
-    }));
-  } else {
-    dispatch(setCurrentUser({
-      authenticated: true,
-      data: { ...user, token: user.token },
-    }));
-  }
-};
-
 export const signup = (params) => async (dispatch) => {
   const path = 'v1/users';
   try {
     const res = await sendRequest('post', path, params);
-    // const { user, token } = await res.data;
-    // setCookie('token', token);
-    // localStorage.setItem('token', token);
-    // setAuthorizationToken(token);
-    // setUserInStore({ ...user, token }, dispatch);
     return res;
   } catch (err) {
     dispatch(setError(err.response.data.error));
@@ -47,7 +25,7 @@ export const signup = (params) => async (dispatch) => {
   }
 };
 
-export const login = (params) => async (dispatch, getState) => {
+export const login = (params) => async (dispatch) => {
   const path = 'v1/login';
 
   try {
@@ -56,8 +34,6 @@ export const login = (params) => async (dispatch, getState) => {
     setAuthorizationToken(token);
     setCookie('token', token);
     localStorage.setItem('token', token);
-    // await setUserInStore({ ...user, token }, dispatch);
-    // const currentUserData = getState().currentUser.data;
     dispatch(setCurrentUser({
       authenticated: true,
       data: user,
