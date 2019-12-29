1b81b042f960e16cce6b44ab96c5422875e4ccf8
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-29 07:45:50 +0700

    fix setClinic bug

--- src/components/Authenticated/Practitioner/PractitionerProfile.js
+++ src/components/Authenticated/Practitioner/PractitionerProfile.js
@@ -1,20 +1,20 @@
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { setClinic } from '../../../store/actions/clinic';
 import { setPractitioner } from '../../../store/actions/practitioners';
 import Booking from '../Booking/Booking';
 import PractitionerCard from './PractitionerCard';
 import PractitionerDetailsCard from './PractitionerDetailsCard';
 
-const PractitionerProfile = ({ setPractitioner, role }) => {
-
+const PractitionerProfile = ({ setPractitioner, role, setClinic }) => {
   useEffect(
     () => () => {
       setClinic({});
     },
     [],
   );
-  
+
   return (
     <div className="profile">
       <div>
@@ -32,10 +32,10 @@ PractitionerProfile.propTypes = {
   role: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   role: state.currentUser.data.role,
 });
 
-export default connect(mapStateToProps, { setPractitioner })(
+export default connect(mapStateToProps, { setPractitioner, setClinic })(
   PractitionerProfile,
 );
