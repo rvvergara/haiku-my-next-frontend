a6120035c9052933b8088e15f39ce316b318a2a5
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-29 17:00:03 +0700

    style MultipleInput

--- src/components/Layouts/Header/CollapsibleNav.js
+++ src/components/Layouts/Header/CollapsibleNav.js
@@ -1,12 +1,12 @@
-import { connect } from 'react-redux';
 import Router from 'next/router';
 import PropTypes from 'prop-types';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FiLogOut } from 'react-icons/fi';
-import Nav from 'react-bootstrap/Nav';
-import PractitionerNavLinks from './PractitionerNavLinks';
-import PatientNavLinks from './PatientNavLinks';
+import { connect } from 'react-redux';
 import { logout } from '../../../store/thunks/user';
+import PatientNavLinks from './PatientNavLinks';
+import PractitionerNavLinks from './PractitionerNavLinks';
 
 export const CollapsibleNav = ({ currentUserData, logout }) => {
   const handleLogout = () => {
@@ -14,37 +14,35 @@ export const CollapsibleNav = ({ currentUserData, logout }) => {
     Router.push('/');
   };
 
-  const { firstName, role } = currentUserData;
+  const { role } = currentUserData;
   return (
     <>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse
-        id='basic-navbar-nav'
-        className='logged-header-collapsible-nav'
+        id="basic-navbar-nav"
+        className="logged-header-collapsible-nav"
       >
-        <Nav className='ml-auto header-nav'>
-          <div className='user-menu'>
-            {
-              role === 'PRACTITIONER'
-            ? <PractitionerNavLinks />
-            : <PatientNavLinks />
-            }
+        <Nav className="ml-auto header-nav">
+          <div className="user-menu">
+            {role === 'PRACTITIONER' ? (
+              <PractitionerNavLinks />
+            ) : (
+              <PatientNavLinks />
+            )}
           </div>
-          <div className='header__links__welcome'>
-            <strong className='logged-header-greeting'>
-              Welcome
-              {' '}
-              {role === 'PRACTITIONER' ? 'Dr. ' : ''}
-              {firstName}
+          <div className="header__links__welcome">
+            <strong className="logged-header-greeting">
+              Welcome {role === 'PRACTITIONER' ? 'Dr. ' : ''}
+              {currentUserData.practitioner.firstName}
             </strong>
           </div>
           <Nav.Link
-            type='button'
-            className='theme-button inverse-theme-button'
+            type="button"
+            className="theme-button inverse-theme-button"
             onClick={handleLogout}
           >
-            <FiLogOut className='nav-icons logout-icon' />
-                Logout
+            <FiLogOut className="nav-icons logout-icon" />
+            Logout
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
@@ -57,7 +55,7 @@ CollapsibleNav.propTypes = {
   logout: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   currentUserData: state.currentUser.data,
 });
 
