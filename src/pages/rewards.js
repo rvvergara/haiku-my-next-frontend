7eb4362b333d8df1d16f71bd1a04ccda460f7c99
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-31 16:10:29 +0700

    add rewards & search pages

---
+++ src/pages/rewards.js
@@ -0,0 +1,8 @@
+import Layout from '../components/Layouts/Layout';
+
+const rewards = () => (
+  <Layout title="Bookings">
+    <div>Rewards page is under construction</div>
+  </Layout>
+);
+export default rewards;
---
+++ src/pages/search.js
@@ -0,0 +1,8 @@
+import Layout from '../components/Layouts/Layout';
+
+const search = () => (
+  <Layout title="Bookings">
+    <div>Search page is under construction</div>
+  </Layout>
+);
+export default search;
