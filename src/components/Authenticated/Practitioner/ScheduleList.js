91e22b56a0aa2507be4e07392035d20fd0f126d7
Zeha Irawan <jangkaringbumi@gmail.com>
2020-01-02 11:37:53 +0700

    modify calendar

--- src/components/Authenticated/Practitioner/ScheduleList.js
+++ src/components/Authenticated/Practitioner/ScheduleList.js
@@ -1,14 +1,18 @@
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { connect } from 'react-redux';
 import { listAvailabilies } from '../../../store/actions/availability';
 
+const localizer = momentLocalizer(moment);
+
 const ScheduleList = ({ availabilities, listAvailabilies }) => {
-  useEffect(() => () => listAvailabilies([]),
-   []);
+  useEffect(() => () => listAvailabilies([]), []);
   return (
     <div className="scheduler-container">
-      {availabilities.map((availability) => (
+      {/* {availabilities.map((availability) => (
         <div key={availability.id}>
           <div>
             <strong>{availability.date}</strong>
@@ -24,7 +28,21 @@ const ScheduleList = ({ availabilities, listAvailabilies }) => {
           </div>
           <div>{availability.booked ? 'Booked' : ''}</div>
         </div>
-      ))}
+      ))} */}
+      <Calendar
+        localizer={localizer}
+        events={[
+          {
+            title: '10am-2pm',
+            allDay: false,
+            start: new Date(2020, 0, 1, 10, 0), // 10.00 AM
+            end: new Date(2020, 0, 1, 14, 0), // 2.00 PM
+          },
+        ]}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   );
 };
@@ -34,7 +52,7 @@ ScheduleList.propTypes = {
   listAvailabilies: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   availabilities: state.availabilities,
 });
 
