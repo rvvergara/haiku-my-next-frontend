69c3a41e56faaed359b88ce17aea1ec24006d5d8
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-03 10:33:02 +0800

    Debug ScheduleList calendar

--- src/components/Authenticated/Practitioner/ScheduleList.js
+++ src/components/Authenticated/Practitioner/ScheduleList.js
@@ -9,7 +9,6 @@ import { listAvailabilies } from '../../../store/actions/availability';
 const localizer = momentLocalizer(moment);
 
 const ScheduleList = ({ availabilities, listAvailabilies }) => {
-
   useEffect(() => () => listAvailabilies([]), []);
   return (
     <div className="scheduler-container">
@@ -32,12 +31,15 @@ const ScheduleList = ({ availabilities, listAvailabilies }) => {
       ))} */}
       <Calendar
         localizer={localizer}
-        events={availabilities.map(avail => {
+        events={availabilities.map((avail) => {
+          console.log('USING NEW DATE', new Date('January 02, 2020,03:24:00'));
+          console.log('START TIME', moment(`${avail.date} ${avail.startTime}`, 'LTS').format('ddd MMM DD YYYY LT'));
+          console.log('END TIME', moment(avail.endTime, 'LTS').format('YYYY-MM-DD HH:MM:SS'));
           return {
             title: `${avail.startTime} - ${avail.endTime}`,
             allDay: false,
-            start: new Date(2020, 0, 1, 10, 0), // 10.00 AM
-            end: new Date(2020, 0, 1, 14, 0), // 2.00 PM
+            start: moment(`${avail.date} ${avail.startTime}`, 'LTS').format('YYYY-MM-DD, HH:MM:SS'), // 10.00 AM
+            end: moment(`${avail.date} ${avail.endTime}`, 'LTS').format('YYYY-MM-DD, HH:MM:SS'), // 2.00 PM
           };
         })}
         startAccessor="start"
@@ -53,7 +55,7 @@ ScheduleList.propTypes = {
   listAvailabilies: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   availabilities: state.availabilities,
 });
 
