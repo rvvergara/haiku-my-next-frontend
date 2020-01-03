27e69e02bb1d5745c28f3c3e237225763fc36f52
Zeha Irawan <jangkaringbumi@gmail.com>
2020-01-03 14:28:06 +0700

    add PractitionerList

---
+++ src/pages/practitioners.js
@@ -0,0 +1,8 @@
+import Layout from '../components/Layouts/Layout';
+
+const practitioners = () => (
+  <Layout title="Doctors list">
+    <div>List of doctors is under construction</div>
+  </Layout>
+);
+export default practitioners;
