a19320e277c21afed95a65ce45b892204665a6a3
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-03 11:11:39 +0800

    Fetch practitioner availabilities only in Booking.js

--- src/components/Authenticated/Booking/Booking.js
+++ src/components/Authenticated/Booking/Booking.js
@@ -1,27 +1,47 @@
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import BookingSelection from './BookingSelection';
 import BookingForm from './BookingForm';
+import { fetchPractitionerAvailabilities } from '../../../store/thunks/availability';
+import { listAvailabilies } from '../../../store/actions/availability';
 
-const Booking = ({ settingAppointment }) => (
-  <div className="booking-container">
-    <div className="booking-form-header">
-      <h5>Book an Appointment</h5>
-    </div>
-    {
+const Booking = ({
+  settingAppointment, fetchPractitionerAvailabilities,
+   displayedPractitioner,
+   listAvailabilies,
+}) => {
+  useEffect(() => {
+    fetchPractitionerAvailabilities(displayedPractitioner.id);
+    return () => listAvailabilies([]);
+  }, []);
+  return (
+    <div className="booking-container">
+      <div className="booking-form-header">
+        <h5>Book an Appointment</h5>
+      </div>
+      {
       settingAppointment
       ? <BookingForm />
       : <BookingSelection />
     }
-  </div>
+    </div>
   );
+};
 
 Booking.propTypes = {
-  // settingAppointment: PropTypes.func.isRequired,
+  displayedPractitioner: PropTypes.instanceOf(Object).isRequired,
+  fetchPractitionerAvailabilities: PropTypes.func.isRequired,
+  settingAppointment: PropTypes.func.isRequired,
+  listAvailabilies: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   settingAppointment: state.settingAppointment,
+  displayedPractitioner: state.displayedPractitioner.id,
 });
 
-export default connect(mapStateToProps)(Booking);
+export default connect(mapStateToProps, {
+   fetchPractitionerAvailabilities,
+   listAvailabilies,
+  })(Booking);
--- src/pages/practitioners/[id].js
+++ src/pages/practitioners/[id].js
@@ -2,7 +2,6 @@ import { connect } from 'react-redux';
 import PractitionerProfile from '../../components/Authenticated/Practitioner/PractitionerProfile';
 import Layout from '../../components/Layouts/Layout';
 import { fetchOnePractitioner } from '../../store/thunks/practitioner';
-import { fetchPractitionerAvailabilities } from '../../store/thunks/availability';
 
 const DoctorProfile = () => (
   <Layout title="Practitioner Profile">
@@ -14,8 +13,6 @@ DoctorProfile.getInitialProps = async (ctx) => {
   const { store, query } = ctx;
   const { dispatch } = store;
   await dispatch(fetchOnePractitioner(query.id));
-  const practitionerId = store.getState().displayedPractitioner.id;
-  await dispatch(fetchPractitionerAvailabilities(practitionerId, '', ''));
 };
 
 export default connect((state) => state)(DoctorProfile);
