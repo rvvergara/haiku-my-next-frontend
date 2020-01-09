a7fdbad6af3b6d06fe7b11fc3ad4cc8299a3ad11
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-09 11:44:14 +0800

    Add change language feature in VisitorHeader

--- src/components/VisitorContent/VisitorHeader.js
+++ src/components/VisitorContent/VisitorHeader.js
@@ -1,50 +1,82 @@
+import { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Link from 'next/link';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { withTranslation } from '../../../i18n';
+import { i18n, withTranslation } from '../../../i18n';
+import { setLanguage } from '../../store/actions/language';
+import localLanguages from '../../utils/languange';
 
-const VisitorHeader = ({ t }) => (
-  <header>
-    <Navbar bg="light" expand="sm">
-      <Navbar.Brand href="/">
-        <img
-          src="./static/igaku_social_logo_text.png"
-          className="header-logo-small"
-          alt="Igaku Logo"
-        />
+const VisitorHeader = ({ t, localLang, setLanguage }) => {
+  const [local, setLocal] = useState(localLanguages[localLang]);
 
-        <img
-          src="./static/igaku_logo_side_by_side.png"
-          className="header-logo-text"
-          alt="Igaku Logo"
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Link href="/login">
-            <Nav.Link
-              className="theme-button"
-              onClick={() => localStorage.clear()}
-              href="/login"
-            >
-              {t('login')}
-            </Nav.Link>
-          </Link>
-          <Link href="/signup">
-            <Nav.Link
-              className="theme-button"
-              type="button"
-              href="/signup"
-              onClick={() => localStorage.clear()}
-            >
-              {t('signup')}
-            </Nav.Link>
-          </Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </header>
+  useEffect(() => {
+    setLocal(localLang);
+  }, [localLang]);
+
+  const handleChange = (e) => {
+    setLocal(e.target.value);
+    setLanguage(e.target.value);
+    i18n.changeLanguage(e.target.value);
+  };
+  return (
+    <header>
+      <Navbar bg="light" expand="sm">
+        <Navbar.Brand href="/">
+          <img
+            src="./static/igaku_social_logo_text.png"
+            className="header-logo-small"
+            alt="Igaku Logo"
+          />
+
+          <img
+            src="./static/igaku_logo_side_by_side.png"
+            className="header-logo-text"
+            alt="Igaku Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <select onChange={handleChange} value={local}>
+              <option value="id">Bahasa Indonesia</option>
+              <option value="en">English</option>
+            </select>
+            <Link href="/login">
+              <Nav.Link
+                className="theme-button"
+                onClick={() => localStorage.clear()}
+                href="/login"
+              >
+                {t('login')}
+              </Nav.Link>
+            </Link>
+            <Link href="/signup">
+              <Nav.Link
+                className="theme-button"
+                type="button"
+                href="/signup"
+                onClick={() => localStorage.clear()}
+              >
+                {t('signup')}
+              </Nav.Link>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
 );
+};
 
-export default withTranslation('visitorHeader')(VisitorHeader);
+VisitorHeader.propTypes = {
+  t: PropTypes.func.isRequired,
+  localLang: PropTypes.string.isRequired,
+  setLanguage: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  localLang: state.language,
+});
+
+export default connect(mapStateToProps, { setLanguage })(withTranslation('visitorHeader')(VisitorHeader));
