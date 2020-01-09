e6317720e391abaee589830c06c3c4229b5c3b7d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-09 09:19:22 +0800

    Wrap practitioner id page in withTranslation

--- src/pages/practitioners/[id].js
+++ src/pages/practitioners/[id].js
@@ -2,9 +2,10 @@ import { connect } from 'react-redux';
 import PractitionerProfile from '../../components/Authenticated/Practitioner/PractitionerProfile';
 import Layout from '../../components/Layouts/Layout';
 import { fetchOnePractitioner } from '../../store/thunks/practitioner';
+import { withTranslation } from '../../../i18n';
 
-const DoctorProfile = () => (
-  <Layout title="Practitioner Profile">
+const DoctorProfile = ({ t }) => (
+  <Layout title={t('Practitioner Profile')}>
     <PractitionerProfile />
   </Layout>
 );
@@ -13,6 +14,7 @@ DoctorProfile.getInitialProps = async (ctx) => {
   const { store, query } = ctx;
   const { dispatch } = store;
   await dispatch(fetchOnePractitioner(query.id));
+  return { namespacesRequired: ['practitionerPage', 'patientNavLink'] };
 };
 
-export default connect((state) => state)(DoctorProfile);
+export default connect((state) => state)(withTranslation('practitionerPage')(DoctorProfile));
