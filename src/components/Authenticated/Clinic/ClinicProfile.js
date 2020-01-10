a4d5c9cade3586fc2cf042d6d3ecd427f3c86e0c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-10 16:10:12 +0800

    Translate clinic profile page and component

--- src/components/Authenticated/Clinic/ClinicProfile.js
+++ src/components/Authenticated/Clinic/ClinicProfile.js
@@ -3,13 +3,16 @@ import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { setClinic } from '../../../store/actions/clinic';
 import PractitionerListClinic from '../Practitioner/PractitionerListClinic';
+import { withTranslation } from '../../../../i18n';
 
-const ClinicProfile = ({ setClinic, clinic, practitioner }) => {
-  const { name, address, postalCode, image, category, openingHours } = clinic;
+const ClinicProfile = ({
+ setClinic, clinic, practitioner, t,
+}) => {
+  const {
+ name, address, postalCode, image, category, openingHours,
+} = clinic;
 
-  const defaultPic =
-    'https://images.unsplash.com/photo-1533042789716-e9a9c97cf4ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
-
+  const defaultPic = 'https://images.unsplash.com/photo-1533042789716-e9a9c97cf4ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
   // const handleAdd = () => {
   //   addPractitionerToClinic(clinic.id, { practitionerId: practitioner.id });
   // };
@@ -42,10 +45,14 @@ const ClinicProfile = ({ setClinic, clinic, practitioner }) => {
                 {postalCode}
               </li>
               <ul className="clinic-opening-hours">
-                <h3>Clinic Opening Hours</h3>
+                <h3>{t('opening-hours')}</h3>
                 <li>{openingHours}</li>
               </ul>
-              <h3>Category : {category}</h3>
+              <h3>
+                {t('category')}
+                {' '}
+                {category}
+              </h3>
             </ul>
             {/* <button onClick={handleAdd}>Add me to this clinic</button> */}
           </div>
@@ -60,11 +67,12 @@ const ClinicProfile = ({ setClinic, clinic, practitioner }) => {
 ClinicProfile.propTypes = {
   clinic: PropTypes.instanceOf(Object).isRequired,
   setClinic: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   clinic: state.displayedClinic,
   practitioner: state.currentUser.data.practitioner,
 });
 
-export default connect(mapStateToProps, { setClinic })(ClinicProfile);
+export default connect(mapStateToProps, { setClinic })(withTranslation('clinicProfile')(ClinicProfile));
--- src/pages/clinics/[id].js
+++ src/pages/clinics/[id].js
@@ -3,22 +3,33 @@ import PropTypes from 'prop-types';
 import ClinicProfile from '../../components/Authenticated/Clinic/ClinicProfile';
 import Layout from '../../components/Layouts/Layout';
 import { fetchOneClinic } from '../../store/thunks/clinic';
+import { withTranslation } from '../../../i18n';
 
-
-const ClinicPage = ({ clinic }) => (
-  <Layout title={clinic.name}>
-    <ClinicProfile />
-  </Layout>
-);
+const ClinicPage = ({ clinic, t }) => {
+  const { name } = clinic;
+  return (
+    <Layout title={t('title', { name })}>
+      <ClinicProfile />
+    </Layout>
+  );
+};
 
 ClinicPage.propTypes = {
   clinic: PropTypes.instanceOf(Object).isRequired,
+  t: PropTypes.func.isRequired,
 };
 
 ClinicPage.getInitialProps = async (ctx) => {
   const { store } = ctx;
   const clinic = await store.dispatch(fetchOneClinic(ctx.query.id));
-  return { clinic };
+  return {
+    clinic,
+    namespacesRequired: [
+      'clinicProfile',
+      'practitionerNavLink',
+      'patientNavLink',
+    ],
+  };
 };
 
-export default connect((state) => state, { fetchOneClinic })(ClinicPage);
+export default connect((state) => state, { fetchOneClinic })(withTranslation('clinicProfile')(ClinicPage));
