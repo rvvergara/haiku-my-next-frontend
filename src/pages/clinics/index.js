262b8e194dad99b6090bafe80ff41870ffb930d4
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-10 14:28:05 +0800

    Translate clinics page

--- src/pages/clinics/index.js
+++ src/pages/clinics/index.js
@@ -1,16 +1,31 @@
+import PropTypes from 'prop-types';
 import Layout from '../../components/Layouts/Layout';
 import ClinicList from '../../components/Authenticated/Clinic/ClinicList';
 import { setAuthorizationToken } from '../../utils/api';
+import { withTranslation } from '../../../i18n';
 
-const ClinicsPage = () => {
+const ClinicsPage = ({ t }) => {
   if (process.browser) {
     setAuthorizationToken(localStorage.token);
   }
   return (
-    <Layout title="Clinics">
+    <Layout title={t('title')}>
       <ClinicList />
     </Layout>
   );
 };
 
-export default ClinicsPage;
+ClinicsPage.propTypes = {
+  t: PropTypes.func.isRequired,
+};
+
+ClinicsPage.getInitialProps = () => ({
+  namespacesRequired: [
+    'clincs',
+    'clinicList',
+    'practitionerNavLink',
+    'patientNavLink',
+  ],
+});
+
+export default withTranslation('clinics')(ClinicsPage);
