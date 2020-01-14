f7af1b7ec10699444f1ba222e9e60f5515bb55d1
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-14 16:41:45 +0800

    Clean up video related files

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,15 +1,9 @@
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Layout from '../components/Layouts/Layout';
-import VideoComponent from '../components/Authenticated/VideoComponent';
 
 const VideoPage = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-    <Layout title='Video'>
-      <VideoComponent />
-    </Layout>
-  </MuiThemeProvider>
+  <Layout title='Video'>
+    <h1>Hello</h1>
+  </Layout>
 );
 
 export default VideoPage;
