a4a23a694e96616232ebf4fd986df71d9b4cdb61
Zeha Irawan <jangkaringbumi@gmail.com>
2020-01-14 15:46:38 +0700

    Add component related to video feature

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,8 +1,10 @@
+import VideoChat from '../components/Authenticated/Video/VideoChat';
 import Layout from '../components/Layouts/Layout';
 
 const VideoPage = () => (
-  <Layout title='Video'>
+  <Layout title="Video">
     <h1>Hello</h1>
+    <VideoChat />
   </Layout>
 );
 
