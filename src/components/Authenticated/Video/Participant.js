95fe87ca331a6d79a095212c660ecd98b39063aa
Zeha Irawan <jangkaringbumi@gmail.com>
2020-01-16 09:20:00 +0700

    Disable mute

--- src/components/Authenticated/Video/Participant.js
+++ src/components/Authenticated/Video/Participant.js
@@ -63,7 +63,7 @@ const Participant = ({ participant }) => {
     <div className="participant">
       <h3>{participant.identity}</h3>
       <video ref={videoRef} autoPlay={true} />
-      <audio ref={audioRef} autoPlay={true} muted={true} />
+      <audio ref={audioRef} autoPlay={true} muted={false} />
     </div>
   );
 };
