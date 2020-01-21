3e2aa40b7c309b8cc1102e6a3fb722a3e2c2ab1b
Zeha Irawan <jangkaringbumi@gmail.com>
2020-01-21 14:13:37 +0700

    bring back practitionerProfile in Dashboard

--- src/components/Authenticated/Dashboard.js
+++ src/components/Authenticated/Dashboard.js
@@ -1,19 +1,23 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import PatientNotification from './Booking/PatientNotification';
-import PractitionerNotification from './Booking/PractitionerNotification';
 import PractitionerList from './Practitioner/PractitionerList';
 import PractitionerProfile from './Practitioner/PractitionerProfile';
 
-const renderDashboard = (userRole, notifications) => {
+const renderDashboard = (userRole) => {
   switch (userRole) {
     case 'PRACTITIONER':
       return (
-        <div />
+        <div>
+          {' '}
+          <PractitionerProfile />
+        </div>
       );
     case 'PATIENT':
       return (
-        <div />
+        <div>
+          {' '}
+          <PractitionerList />
+        </div>
       );
     default:
       return null;
@@ -29,7 +33,7 @@ Dashboard.propTypes = {
   notifications: PropTypes.instanceOf(Object).isRequired,
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   userRole: state.currentUser.data.role,
   notifications: state.notifications,
 });
