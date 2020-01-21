4d4c71a25e5af66c096fb5703b137b68bad4a46d
Zeha Irawan <jangkaringbumi@gmail.com>
2020-01-21 15:03:34 +0700

    add notification in practitionerNavLinks

--- src/components/Layouts/Header/PractitionerNavLinks.js
+++ src/components/Layouts/Header/PractitionerNavLinks.js
@@ -1,53 +1,51 @@
 import Link from 'next/link';
 import Nav from 'react-bootstrap/Nav';
-import {
- FaCalendarAlt,
-} from 'react-icons/fa';
+import { FaCalendarAlt } from 'react-icons/fa';
 import { GiHealthNormal } from 'react-icons/gi';
 import { GoPerson } from 'react-icons/go';
-import { MdSchedule, MdFeedback } from 'react-icons/md';
+import { MdFeedback, MdSchedule } from 'react-icons/md';
 import { withTranslation } from '../../../../i18n';
-
+import Notification from '../../Authenticated/Notification/Notification';
 
 const PractitionerNavLinks = ({ t }) => (
   <>
-    <Link href='/account/edit'>
-      <Nav.Link href='/account/edit' className='header-navlinks'>
-        <GoPerson className='nav-icons' />
-        <span className='header-navlinks-text'>{t('settings')}</span>
+    <Link href="/account/edit">
+      <Nav.Link href="/account/edit" className="header-navlinks">
+        <GoPerson className="nav-icons" />
+        <span className="header-navlinks-text">{t('settings')}</span>
       </Nav.Link>
     </Link>
-    <Link href='/profile/edit'>
-      <Nav.Link href='/profile/edit' className='header-navlinks'>
-        <GoPerson className='nav-icons' />
-        <span className='header-navlinks-text'>{t('profile')}</span>
+    <Link href="/profile/edit">
+      <Nav.Link href="/profile/edit" className="header-navlinks">
+        <GoPerson className="nav-icons" />
+        <span className="header-navlinks-text">{t('profile')}</span>
       </Nav.Link>
     </Link>
-    <Link href='/schedule'>
-      <Nav.Link href='/schedule' className='header-navlinks'>
-        <FaCalendarAlt className='nav-icons' />
-        <span className='header-navlinks-text'>{t('schedule')}</span>
+    <Link href="/schedule">
+      <Nav.Link href="/schedule" className="header-navlinks">
+        <FaCalendarAlt className="nav-icons" />
+        <span className="header-navlinks-text">{t('schedule')}</span>
       </Nav.Link>
     </Link>
-    <Link href='/bookings'>
-      <Nav.Link href='/bookings' className='header-navlinks'>
-        <MdSchedule className='nav-icons' />
-        <span className='header-navlinks-text'>{t('bookings')}</span>
+    <Link href="/bookings">
+      <Nav.Link href="/bookings" className="header-navlinks">
+        <MdSchedule className="nav-icons" />
+        <span className="header-navlinks-text">{t('bookings')}</span>
       </Nav.Link>
     </Link>
-    <Link href='/clinics'>
-      <Nav.Link href='/clinics' className='header-navlinks'>
-        <GiHealthNormal className='nav-icons' />
-        <span className='header-navlinks-text'>{t('clinics')}</span>
+    <Link href="/clinics">
+      <Nav.Link href="/clinics" className="header-navlinks">
+        <GiHealthNormal className="nav-icons" />
+        <span className="header-navlinks-text">{t('clinics')}</span>
       </Nav.Link>
     </Link>
-    <Link href='/feedback'>
-      <Nav.Link href='/feedback' className='header-navlinks prominent'>
-        <MdFeedback className='nav-icons' />
-        <span className='header-navlinks-text'>{t('feedback')}</span>
+    <Link href="/feedback">
+      <Nav.Link href="/feedback" className="header-navlinks prominent">
+        <MdFeedback className="nav-icons" />
+        <span className="header-navlinks-text">{t('feedback')}</span>
       </Nav.Link>
     </Link>
-
+    <Notification />
   </>
 );
 
