5e2fff38b924a9ddc6bfe0dec700cb6900911434
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-24 14:30:12 +0800

    Show only upcoming appointment to the dot

--- src/store/thunks/upcomingAppointment.js
+++ src/store/thunks/upcomingAppointment.js
@@ -1,16 +1,22 @@
+import moment from 'moment';
 import { sendRequest } from '../../utils/api';
 import setError from '../actions/error';
 import { listUpcomingAppointment } from '../actions/upcomingAppointment';
+import { localizeBookingSlot } from '../../utils/localize';
 
 export const fetchUpcomingAppointment = (
   role,
   profileId,
-) => async dispatch => {
-  const path = `v1/${role.toLowerCase()}s/${profileId}/booking-slots?include=patient,practitioner&fromDate=01-01-2020&toDate=01-31-2020&status=CONFIRMED`;
+) => async (dispatch) => {
+  const fromDate = moment().format('MM-DD-YYYY');
+  const toDate = moment().endOf('isoWeek').format('MM-DD-YYYY');
+  const path = `v1/${role.toLowerCase()}s/${profileId}/booking-slots?include=patient,practitioner&fromDate=${fromDate}&toDate=${toDate}&status=CONFIRMED`;
   try {
     const res = await sendRequest('get', path);
-    const bookingSlots = res.data.booking_slots;
-    return dispatch(listUpcomingAppointment(bookingSlots));
+    const bookingSlots = res.data.booking_slots
+      .filter((slot) => moment.utc(slot.endTime) >= moment.utc());
+    const localizedSlots = bookingSlots.map((slot) => localizeBookingSlot(slot));
+    return dispatch(listUpcomingAppointment(localizedSlots));
   } catch (err) {
     setError(err);
   }
