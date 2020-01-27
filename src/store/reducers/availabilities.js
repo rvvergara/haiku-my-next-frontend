546df828b3f45cfaf81ed1ccbaef34e0087371db
Zeha Irawan <jangkaringbumi@gmail.com>
2020-01-27 14:25:58 +0700

    fix legend color bugs in calendar component

--- src/store/reducers/availabilities.js
+++ src/store/reducers/availabilities.js
@@ -1,11 +1,11 @@
 import uuid from 'uuid';
 import {
   ADD_AVAILABILITY,
-  LIST_AVAILABILITIES,
-  REMOVE_AVAILABILITY,
   BOOK_AVAILABILITY,
   CONFIRM_BOOKING,
+  LIST_AVAILABILITIES,
   REJECT_BOOKING,
+  REMOVE_AVAILABILITY,
 } from '../actions/types';
 
 // Temporarily setting id for new availabilities until we connect to backend
@@ -15,35 +15,48 @@ export default (state = [], action) => {
     case LIST_AVAILABILITIES:
       return action.availabilities;
     case ADD_AVAILABILITY:
-      return [...state, { ...action.availability, id: uuid(), booked: false }];
+      return [
+        ...state,
+        { ...action.availability, id: uuid(), patientId: null },
+      ];
     case REMOVE_AVAILABILITY:
-      return state.filter((avail) => avail.id !== action.id);
-    case BOOK_AVAILABILITY:
-      {
-        const indexToBook = state.findIndex((avail) => avail.id === action.availabilityId);
-        const bookedSlot = { ...state[indexToBook], patientId: action.patientId, remarks: action.remarks };
-        const newState = [...state];
-        newState[indexToBook] = bookedSlot;
-        return newState;
-      }
-      case CONFIRM_BOOKING:
-        {
-          const indexToConfirm = state.findIndex((slot) => slot.id === action.bookingId);
+      return state.filter(avail => avail.id !== action.id);
+    case BOOK_AVAILABILITY: {
+      const indexToBook = state.findIndex(
+        avail => avail.id === action.availabilityId,
+      );
+      const bookedSlot = {
+        ...state[indexToBook],
+        patientId: action.patientId,
+        remarks: action.remarks,
+      };
+      const newState = [...state];
+      newState[indexToBook] = bookedSlot;
+      return newState;
+    }
+    case CONFIRM_BOOKING: {
+      const indexToConfirm = state.findIndex(
+        slot => slot.id === action.bookingId,
+      );
 
-          const newState = [...state];
-          const confirmedSlot = { ...newState[indexToConfirm], status: 'CONFIRMED' };
-          newState[indexToConfirm] = confirmedSlot;
-          return newState;
-        }
-        case REJECT_BOOKING:
-        {
-          const indexToConfirm = state.findIndex((slot) => slot.id === action.bookingId);
+      const newState = [...state];
+      const confirmedSlot = {
+        ...newState[indexToConfirm],
+        status: 'CONFIRMED',
+      };
+      newState[indexToConfirm] = confirmedSlot;
+      return newState;
+    }
+    case REJECT_BOOKING: {
+      const indexToConfirm = state.findIndex(
+        slot => slot.id === action.bookingId,
+      );
 
-          const newState = [...state];
-          const rejectedSlot = { ...newState[indexToConfirm], status: 'REJECTED' };
-          newState[indexToConfirm] = rejectedSlot;
-          return newState;
-        }
+      const newState = [...state];
+      const rejectedSlot = { ...newState[indexToConfirm], status: 'REJECTED' };
+      newState[indexToConfirm] = rejectedSlot;
+      return newState;
+    }
     default:
       return state;
   }
