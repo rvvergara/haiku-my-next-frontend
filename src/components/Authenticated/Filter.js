b861f911828153e0b6ae1042bdc033b6d27cb81f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-28 14:34:49 +0800

    Fix filter display

--- src/components/Authenticated/Filter.js
+++ src/components/Authenticated/Filter.js
@@ -1,28 +1,49 @@
+import { useState } from 'react';
+
 const Filter = () => {
+  const [specialty, setSpecialty] = useState('Any');
+  const [insurance, setInsurance] = useState('Any');
+  const [experience, setExperience] = useState('Any');
   return (
     <div className="filter">
       <h6>Filter Results</h6>
-      <label htmlFor="">Filter by Speciality</label>
-
-      <select className="filter-select">
-        <option value="any" selected="selected">
+      <label htmlFor="specialty">Filter by Speciality</label>
+      <select
+        id='specialty'
+        className="filter-select"
+        value={specialty}
+        onChange={(e) => setSpecialty(e.target.value)}
+      >
+        <option value="any">
           Any
         </option>
-        <option value="volvo">Gastroenterology</option>
-        <option value="saab">Metabolic surgery</option>
+        <option value="gastroenterology">
+          Gastroenterology
+        </option>
+        <option value="metabolic-surgery">Metabolic surgery</option>
       </select>
-      <label htmlFor="">Insurance Accepted</label>
-      <select className="filter-select">
-        <option value="any" selected="selected">
+      <label htmlFor="insurance">Insurance Accepted</label>
+      <select
+        id="insurance"
+        className="filter-select"
+        value={insurance}
+        onChange={(e) => setInsurance(e.target.value)}
+      >
+        <option value="any">
           Any
         </option>
         <option value="prudential">Prudential</option>
         <option value="aia">AIA</option>
         <option value="aviva">AVIVA</option>
       </select>
-      <label htmlFor="">Years of Experience</label>
-      <select className="filter-select">
-        <option value="any" selected="selected">
+      <label htmlFor="experience">Years of Experience</label>
+      <select
+        id="experience"
+        className="filter-select"
+        value={experience}
+        onChange={(e) => setExperience(e.target.value)}
+      >
+        <option value="any">
           Any
         </option>
         <option value="5-10">5-10 years</option>
