b812e21b0a8deb5bbe25eec3cd68d2995f6c067a
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-01-28 10:54:01 +0800

    Add a fallback to test production build in dev server

--- src/utils/secrets-manager.js
+++ src/utils/secrets-manager.js
@@ -3,7 +3,6 @@ const AWS = require('aws-sdk');
 const region = 'ap-southeast-1';
 const secretName = 'Twilio-Credentials';
 let secret;
-let decodedBinarySecret;
 
 const client = new AWS.SecretsManager({
   region,
@@ -12,14 +11,14 @@ const client = new AWS.SecretsManager({
 const getAPIKey = async () => new Promise((resolve, reject) => {
     client.getSecretValue({ SecretId: secretName }, (err, data) => {
       if (err) {
-        if (err.code === 'DecryptionFailureException') { reject(err); } else if (err.code === 'InternalServiceErrorException') { reject(err); } else if (err.code === 'InvalidParameterException') { reject(err); } else if (err.code === 'InvalidRequestException') { reject(err); } else if (err.code === 'ResourceNotFoundException') { rej(err); }
+        if (err.code === 'DecryptionFailureException') { reject(err); } else if (err.code === 'InternalServiceErrorException') { reject(err); } else if (err.code === 'InvalidParameterException') { reject(err); } else if (err.code === 'InvalidRequestException') { reject(err); } else if (err.code === 'ResourceNotFoundException') { reject(err); }
       } else if ('SecretString' in data) {
           secret = data.SecretString;
         } else {
         const buff = new Buffer(data.SecretBinary, 'base64');
         decodedBinarySecret = buff.toString('ascii');
       }
-      const twilioCredentials = JSON.parse(secret);
+      const twilioCredentials = secret ? JSON.parse(secret) : process.env;
       process.env.TWILIO_ACCOUNT_SID = twilioCredentials.TWILIO_ACCOUNT_SID;
       process.env.TWILIO_API_KEY = twilioCredentials.TWILIO_API_KEY;
       process.env.TWILIO_API_SECRET = twilioCredentials.TWILIO_API_SECRET;
